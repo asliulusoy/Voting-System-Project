8323fd6a13226ca8efd4dd35c0c1b615312db3b0
aslı <wyneshalitie@gmail.com>
2023-11-28 18:42:28 +0300

    database.js güncellendi

--- database.js
+++ database.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 const conn = () => {
     mongoose.connect(process.env.MONGODB_URI, {
         dbName: "Voting-System",
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
     }).then(() => {
         console.log("Successfully connected to the Voting-System-Project Database");
     }).catch((err) => {
