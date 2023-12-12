7cd3f9b0613e24f57fccc475741bed49a72d597c
aslı <wyneshalitie@gmail.com>
2023-12-12 17:32:43 +0300

    gerekli validation duzeltmeleri yapildi. sorunsuz calisiyor. css eksik

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,10 +5,13 @@ import bcrypt from 'bcrypt';
 const createUser = async (req, res) => {
     try {
         const user = await User.create(req.body);
-      res.redirect("/login");
+      res.status(200).json({user: user._id});
     } catch (error) {
 
       let errors2 = {};
+      if (error.code === 11000) {
+        errors2.email = 'The Email is already registered';
+      }
       if (error.name ==="ValidationError"){
         Object.keys(error.errors).forEach((key)=>{
           errors2[key]= error.errors[key].message;
