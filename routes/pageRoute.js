0449de6305d45e9f1c53ce87e66044ae3b69cbd7
aslı <wyneshalitie@gmail.com>
2023-12-19 17:40:57 +0300

    .

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -10,10 +10,7 @@ router.route("/signup").get(pageController.getIndexPage);
 router.route("/HowtoVote").get(pageController.getBLVotingtPage);
 router.route("/contact").get(pageController.getContactPage);
 router.route("/contact").post(pageController.sendMail);
-<<<<<<< HEAD
-=======
 router.route("/about").get(pageController.getAboutPage);
->>>>>>> asli
 router.route("/logout").get(pageController.getLogoutPage);
-router.route("/about").get(pageController.getAboutPage);
+
 export default router;
