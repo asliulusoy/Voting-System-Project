e88a3b402c9179e9df2fa4b34f1e864ffe9e7d85
aslı <wyneshalitie@gmail.com>
2023-12-19 15:50:39 +0300

    about route ve controller duzenlendi

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -10,7 +10,7 @@ router.route("/signup").get(pageController.getIndexPage);
 router.route("/HowtoVote").get(pageController.getBLVotingtPage);
 router.route("/contact").get(pageController.getContactPage);
 router.route("/contact").post(pageController.sendMail);
-router.route("/about").get(pageController.getContactPage);
+router.route("/about").get(pageController.getAboutPage);
 router.route("/logout").get(pageController.getLogoutPage);
 
 export default router;
